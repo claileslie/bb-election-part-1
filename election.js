50a5799a20bd5a3a8e13d0fc1408b31adbc0fbad
Claire Leslie <c.leslie@live.ca>
2017-02-17 14:24:39 -0500

    create request to loop over BB candidates and display each name and vote count in an ul

--- election.js
+++ election.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
-  // Imagination!
-
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(responseData){
+      $(responseData.candidates).each(function(candidate) {
+        $('<li>').html('Candidate: ' + this.name + '  ' + ' Votes: ' + this.votes).appendTo('#data');
+      });
+    });
 });
